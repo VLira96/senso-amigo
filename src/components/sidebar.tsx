d31165ed2713e807e8f855d2475d101c4795f913
Vlira96 <lira94634@gmail.com>
2024-09-09 21:19:59 -0300

    tome

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,18 @@
 const SideBar = () => {
     return (
-        <aside className="bg-azul-principal w-[250px] h-full">
-
+        <aside className="bg-azul-principal text-white w-[250px] h-full">
+            <div className="">
+                <p>logo principal</p>
+            </div>
+            <div>
+                <p>home</p>
+            </div>
+            <div>
+                <p>chat</p>
+            </div>
+            <div>
+                <p>configurações</p>
+            </div>
         </aside>
     );
 }
