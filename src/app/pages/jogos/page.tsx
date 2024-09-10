bbdb3ab5c859afedbfa7ecc9bbd96e7bb42783c1
Hugo <hugogabriel@rede.ulbra.br>
2024-09-09 21:37:14 -0300

    jogos

--- src/app/pages/jogos/page.tsx
+++ src/app/pages/jogos/page.tsx
@@ -4,6 +4,12 @@ export default function Page() {
     return (
         <div>
             <SideBar />
+            <h1>Jogos</h1>
+            <ul>
+                <li>Jogo da Velha</li>
+                <li>Palavra Cruzada</li>
+                <li>Xadrez</li>
+            </ul>
         </div>
     )
 }
