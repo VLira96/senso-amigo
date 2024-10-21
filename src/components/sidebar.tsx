a2135c5c32dd9298b3262a52cc3ea0f44c9b1bd4
hugbrl09 <hugogabriel@rede.ulbra.br>
2024-10-21 20:38:38 -0300

    SideBar Update

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,20 +1,53 @@
-const SideBar = () => {
+import Link from "next/link";
+
+export default function SideBar() {
     return (
-        <aside className="bg-azul-principal text-white w-[250px] h-full">
-            <div className="">
-                <p>logo principal</p>
+        <>
+            <div id="sidebar" className="bg-azul-principal text-white w-[150px] h-screen">
+                <ul>
+                    <li>
+                        <Link href={'/'}>
+                            <img src="/Firefly_gera_uma_logo_para_um_app_de_acessibilidade_para_autistas__logo_simples__sem_muitos_detalhes-removebg-preview.png" alt="Logo Senso Amigo" width="150px" />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href={'/'}>
+                            <img src="/home-icon.png" alt="Início" />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href={'/pages/chat'}>
+                            <img src="/chat-icon.png" alt="Bate-papo" />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href={'/'}>
+                            <img src="/settings-icon.png" alt="Configurações" />
+                        </Link>
+                    </li>
+                </ul>
             </div>
-            <div>
-                <p>home</p>
-            </div>
-            <div>
-                <p>chat</p>
-            </div>
-            <div>
-                <p>configurações</p>
-            </div>
-        </aside>
-    );
+        </>
+    )
 }
 
-export default SideBar;
+// const SideBar = () => {
+//     return (
+//         <aside className="bg-azul-principal text-white w-[250px] h-full">
+//             <div className="">
+//                 <p>logo principal</p>
+//             </div>
+//             <div>
+//                 <p>home</p>
+//             </div>
+//             <div>
+//                 <p>chat</p>
+//             </div>
+//             <div>
+//                 <p>configurações</p>
+//             </div>
+//         </aside>
+//     );
+// }
+
+// export default SideBar;
