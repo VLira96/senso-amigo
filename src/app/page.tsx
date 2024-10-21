695b9302f4c7f6d508eb10bf5db0728992f46ead
paulogosik <paulogosikmoita@gmail.com>
2024-10-21 19:41:36 -0300

    push home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="flex flex-col-reverse bg-azul-principal h-screen">
       <div className="flex gap-80 bg-white h-[50%] rounded-tr-full">
         <p className="m-20 w-[638px] text-4xl">A plataforma que auxiliará alunos com autismo a melhorar a comunicação com professores e colegas.</p>
-        <div className="mt-16 flex flex-col gap-8 text-7xl">
+        <div className="mt-16 flex flex-col gap-8 text-7xl pr-32  ">
           <button className="bg-azul-secundario text-white rounded-xl px-8 py-3 shadow">Login</button>
           <button className="bg-zinc-300 text-azul-secundario rounded-xl px-8 py-3 shadow-sm">Cadastrar</button>
         </div>
