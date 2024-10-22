7f442f5dc4f2ff435ccd3f4d64af3f06c515a3b6
hugbrl09 <hugogabriel@rede.ulbra.br>
2024-10-21 21:09:49 -0300

    aaaa

--- src/app/pages/amigobot/page.tsx
+++ src/app/pages/amigobot/page.tsx
@@ -4,19 +4,20 @@ export default function AmigoBot() {
     return (
         <div id="container" className="flex">
             <SideBar />
-            <div id="amigobot-chat">
+            <div id="amigobot-chat" className="flex">
                 <img
                     src="/amigobot.webp"
                     alt="logo AmigoBot"
-                    width="250px"
-                    height="250px"
+                    className="h-1/4 rounded-full"
+                    // width="250px"
+                    // height="250px"
                 />
-                <h1>AmigoBot</h1>
-                <input
+                <h1 className="text-8xl font-bold">AmigoBot</h1>
+                {/* <input
                     type="text"
                     placeholder="Mensagem"
                     className="outline outline-2 outline-black outline-offset-2 border-none p-1"
-                />
+                /> */}
             </div>
         </div>
     )
