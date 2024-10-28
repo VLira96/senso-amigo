33816ba40825a6542405b8e147b166e8d68ce099
Lucas Amaral De Freitas <lucasamaraldefreitas5@gmail.com>
2024-10-28 18:55:09 -0300

    teste

---
+++ src/app/pages/avisos/page.tsx
@@ -0,0 +1,15 @@
+import SideBar from "@/components/sidebar";
+
+const Page = () => {
+    return (
+        <div className="h-screen bg-zinc-200 ">
+            <SideBar />
+        </div>
+    );
+}
+
+export default avisos() {
+    return()
+}
+
+export default Page
