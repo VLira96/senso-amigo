9673e387c7d7e3ce93987e6311ac4e1c347459e5
paulogosik <paulogosikmoita@gmail.com>
2024-11-11 19:36:30 -0300

    Update page.tsx

--- src/app/pages/atividades/page.tsx
+++ src/app/pages/atividades/page.tsx
@@ -1,9 +1,12 @@
 const Avisos = () => {
   return (
-      <main className="flex-grow p-4">
-        <h1 className="text-6xl font-semibold text-center text-azul-secundario pt-40">Atividades</h1>
-        {/* Aqui vai o conteúdo da página de avisos */}
-      </main>
+    <div id="container" className="flex flex-col items-center h-full w-full">
+      <div className="flex flex-col items-center"></div>
+        <main className="flex-grow p-4">
+          <h1 className="text-6xl font-semibold text-center text-azul-secundario pt-40">Atividades</h1>
+          {/* Aqui vai o conteúdo da página de avisos */}
+        </main>
+      </div>
   );
 };
 
