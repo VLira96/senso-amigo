f8c0f41e9a06e8287bc1e08a07075f5f922b8ae8
Hugo <hugogabriel@rede.ulbra.br>
2024-11-11 19:39:08 -0300

    Home Update

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -2,10 +2,24 @@ import SideBar from "@/components/sidebar";
 
 const Page = () => {
     return (
-        <div className="flex w-screen h-screen bg-zinc-200 ">
-            <div>
-                <div className="bg-blue-400 h-[300px]">
-                    
+        <div id="container" className="flex flex-col items-center h-full w-full">
+            <div className="flex flex-col items-center">
+                <img
+                    src="/junior-home.png"
+                    alt="foto de perfil"
+                    className="h-32 w-32 rounded-full"
+                />
+                <h1 className="text-4xl font-bold text-blue-600 mt-4">Olá, Junior!</h1>
+            </div>
+            <div className="flex items-center mt-8 w-full max-w-md">
+                <div className="bg-blue-500 text-white p-4 rounded-lg mb-4 shadow-md w-3/4 text-center">
+                    Avisos da escola
+                </div>
+                <div className="bg-blue-500 text-white p-4 rounded-lg mb-4 shadow-md w-3/4 text-center">
+                    Jogos educativos
+                </div>
+                <div className="bg-blue-500 text-white p-4 rounded-lg mb-4 shadow-md w-3/4 text-center">
+                    Videos educativos
                 </div>
             </div>
         </div>
