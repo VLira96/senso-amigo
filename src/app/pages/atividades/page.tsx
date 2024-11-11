cc58e330c88157a75e34afd5ced25ead8f6ca36c
paulogosik <paulogosikmoita@gmail.com>
2024-11-11 19:33:56 -0300

    update atividades

--- src/app/pages/atividades/page.tsx
+++ src/app/pages/atividades/page.tsx
@@ -1,14 +1,9 @@
-import SideBar from "@/components/sidebar";
-
 const Avisos = () => {
   return (
-    <div className="flex">
-      <SideBar />
       <main className="flex-grow p-4">
         <h1 className="text-6xl font-semibold text-center text-azul-secundario pt-40">Atividades</h1>
         {/* Aqui vai o conteúdo da página de avisos */}
       </main>
-    </div>
   );
 };
 
