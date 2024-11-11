1a25e5d835908a1f8b880cc53b615c901744ddaa
Hugo <hugogabriel@rede.ulbra.br>
2024-11-11 20:13:38 -0300

    avisos fix

--- src/app/pages/avisos/page.tsx
+++ src/app/pages/avisos/page.tsx
@@ -17,8 +17,8 @@ export default Page
 const Avisos = () => {
   return (
     <main className="flex-grow relative bg-gray-100 h-full">
-      <div className="pt-40 p-4 flex flex-col items-center">
-        <h1 className="text-5xl font-bold text-azul-secundario mb-6">Avisos!!!</h1>
+      <div className="p-4 flex flex-col items-center">
+        <h1 className="text-5xl font-bold text-azul-secundario mb-32">Avisos!!!</h1>
 
         <div className="flex flex-wrap justify-center gap-4">
           <div className="bg-azul-secundario text-white p-4 rounded-lg w-64 text-center shadow-md">
